00c6bcffe0f4f71c5b9e19d548617a9b7af8ecee
zacmaes <zachary.maes@gmail.com>
2024-02-22 14:02:33 -0600

    added server 500 error handling and not found 404 error handling

--- api.mjs
+++ api.mjs
@@ -10,6 +10,16 @@ app.get('/events', (req, res) => {
     res.status(200).json(events);
 });
 
+// Handle non existent routes
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+// Error handling server errors
+app.use((err, req, res, next) => {
+    console.error(err); // Log error to the server's console
+    res.status(500).json({ error: 'Internal Server Error' });
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
